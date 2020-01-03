efb21d40e89f5561cf76f2bb51b4eb2ee241ee85
Brad Ito <phlogisticfugu@gmail.com>
2020-01-03 00:24:22 -0800

    adding comment about health-check endpoint

--- generators/app/templates/src-koa/api/health-check.js
+++ generators/app/templates/src-koa/api/health-check.js
@@ -2,6 +2,13 @@ const Router = require('@koa/router')
 
 const router = new Router()
 
+/*
+ * DO NOT DELETE this endpoint
+ * it is used in the automated health checks for your app
+ *
+ * Instead, add any services you depend upon, such as database connections
+ */
+
 router.get('/', async (ctx, next) => {
   // TODO add in dependencies needed for sanity checks
   // example: open database connections, connect to APIs
