abae00808437f3f99e7506c20e28a466f1a44c04
Brad Ito <phlogisticfugu@gmail.com>
2020-03-28 22:43:25 -0500

    removing redundant env code

--- generators/app/templates/nuxt.config.prod.js
+++ generators/app/templates/nuxt.config.prod.js
@@ -3,12 +3,6 @@ const appConfig = require('./app.config.json')
 
 nuxtConfig.dev = false
 
-const env = Object.assign(
-  nuxtConfig.env || {},
-  appConfig.prod_env || {}
-)
-nuxtConfig.env = env
-
 /*
  * Inject configured environment variables into the Nuxt universal app
  * Note: These are bundled and sent to the client browser
